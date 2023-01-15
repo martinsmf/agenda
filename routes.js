ee094f8b8a2ccecfd7e7555eed246613a598b887
Matheus Martins <martinsmf00@gmail.com>
2023-01-15 20:28:27 -0300

    login and sign out

--- routes.js
+++ routes.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const homeController = require('./src/controllers/homeController');
-const loginController = require('./src/controllers/loginController');
+const homeController = require("./src/controllers/homeController");
+const loginController = require("./src/controllers/loginController");
 
 function meuMiddleware(req, res, next) {
-  req.session = { nome: 'Matheus', sobrenome: 'Miranda' };
+  req.session = { nome: "Matheus", sobrenome: "Miranda" };
   console.log();
-  console.log('Passei no seu middleware');
+  console.log("Passei no seu middleware");
   console.log();
   next();
 }
 
 // Rotas da home
-route.get('/', homeController.index);
+route.get("/", homeController.index);
 
 //rotas de login
 
-route.get('/login/index', loginController.index);
-route.post('/login/register', loginController.register)
+route.get("/login/index", loginController.index);
+route.post("/login/register", loginController.register);
+route.post("/login/login", loginController.login);
+route.get("/login/logout", loginController.logout);
 
 module.exports = route;
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,19 +1,38 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcryptjs = require('bcryptjs');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcryptjs = require("bcryptjs");
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
 });
 
-const LoginModel = mongoose.model('Login', LoginSchema);
+const LoginModel = mongoose.model("Login", LoginSchema);
 
 class Login {
   constructor(body) {
     this.body = body;
     this.errors = [];
     this.user = null;
+  }
+
+  async login() {
+    this.valida();
+    if (this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if (!this.user) {
+      this.errors.push("Usuário não existe.");
+      this.user = null;
+      return;
+    }
+
+    if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push("Senha inválida.");
+      this.user = null;
+      return;
+    }
   }
 
   async register() {
@@ -27,41 +46,35 @@ class Login {
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-    try {
-      this.user = await LoginModel.create(this.body);
-    } catch (e) {;
-      console.log(e);
-    }
+    this.user = await LoginModel.create(this.body);
   }
 
   async userExist() {
     const user = await LoginModel.findOne({ email: this.body.email });
-
-    if (user) this.errors.push('Usuário já existe.');
-
+    if (user) this.errors.push("Usuário já existe.");
   }
 
   valida() {
     this.cleanUp();
-    if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+    if (!validator.isEmail(this.body.email))
+      this.errors.push("E-mail inválido");
 
     if (this.body.password.length < 3 || this.body.password.length > 50) {
-      this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
+      this.errors.push("A senha precisa ter entre 3 e 50 caracteres.");
     }
   }
 
   cleanUp() {
     for (const key in this.body) {
-      if (typeof this.body[key] !== 'string') {
-        this.body = '';
-     }
+      if (typeof this.body[key] !== "string") {
+        this.body = "";
+      }
     }
 
     this.body = {
       email: this.body.email,
-      password: this.body.password
-    }
-
+      password: this.body.password,
+    };
   }
 }
 
