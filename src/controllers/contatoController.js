7ffa109bbcc823d4338eda6424bbd75ecf6701c0
Matheus Martins <martinsmf00@gmail.com>
2023-01-16 22:04:27 -0300

    .

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -35,3 +35,25 @@ exports.editIndex = async function (req, res) {
 
   res.render("contato", { contato });
 };
+
+exports.edit = async function (req, res) {
+  try {
+    if (!req.params.id) return res.render("404");
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+
+    if (contato.errors.length > 0) {
+      req.flash("erros", contato.errors);
+      req.session.save(() => res.redirect("/contato/index"));
+      return;
+    }
+
+    req.flash("success", "Contato editado com sucesso.");
+    req.session.save(() =>
+      res.redirect(`/contato/index/${contato.contato._id}`)
+    );
+  } catch (e) {
+    console.log(e);
+    res.render("404");
+  }
+};
